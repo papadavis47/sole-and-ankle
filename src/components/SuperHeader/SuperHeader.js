00ccf3606482d195744d5f85915075d498976e4e
John William Davis <jwdavisdeveloper@gmail.com>
2021-10-25 13:20:45 -0700

    Finish exercise 1

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import SearchInput from '../SearchInput';
-import UnstyledButton from '../UnstyledButton';
-import Icon from '../Icon';
+import SearchInput from "../SearchInput";
+import UnstyledButton from "../UnstyledButton";
+import Icon from "../Icon";
 
 const SuperHeader = () => {
   return (
@@ -13,19 +13,30 @@ const SuperHeader = () => {
       <MarketingMessage>
         Free shipping on domestic orders over $75!
       </MarketingMessage>
-      <SearchInput />
-      <HelpLink href="/help">Help</HelpLink>
-      <UnstyledButton>
-        <Icon id="shopping-bag" strokeWidth={1} />
-      </UnstyledButton>
+      <div className='sub-wrapper'>
+        <SearchInput />
+        <HelpLink href='/help'>Help</HelpLink>
+        <UnstyledButton>
+          <Icon id='shopping-bag' strokeWidth={1} />
+        </UnstyledButton>
+      </div>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 16px;
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
+
+  .sub-wrapper {
+    display: flex;
+    justify-content: space-around;
+    gap: 16px;
+  }
 `;
 
 const MarketingMessage = styled.span`
